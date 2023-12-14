f8c991ea4aa2ed64c6f89af4a8244ab22b1ec24e
nOttOD4y <kaungmyat97k@gmail.com>
2023-12-14 22:30:01 +0630

    create Prj

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Header></Header>
+    <div class="container">
+      <Balance></Balance>
+      <InComeExpense></InComeExpense>
+      <TransactionList></TransactionList>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import InComeExpense from './components/InComeExpense.vue';
+import TransactionList from './components/TransactionList.vue';
+
+
+export default {
+  components:{
+    Header,
+    Balance,
+    InComeExpense,
+    TransactionList
+  },
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Balance.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h4>Your Balance</h4>
+        <h1 id="balance">$0.00</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
